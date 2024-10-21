3d6eb0d03e309be9d3f7469c63e70390d53f9b08
Diogo Venade <dmsvenade@gmail.com>
2024-10-21 17:14:32 +0100

    llopen progress

--- lab1/src/application_layer.c
+++ lab1/src/application_layer.c
@@ -1,11 +1,38 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
+    LinkLayer connectionParameters;
+
+    strncpy(connectionParameters.serialPort, serialPort, sizeof(connectionParameters.serialPort) - 1);
+    connectionParameters.serialPort[sizeof(connectionParameters.serialPort) - 1] = '\0';
+
+    if (strcmp(role, "tx") == 0) {
+        connectionParameters.role = LlTx;
+    } else if (strcmp(role, "rx") == 0) {
+        connectionParameters.role = LlRx;
+    } else {
+        printf("Invalid role\n");
+        return;
+    }
+
+    connectionParameters.baudRate = baudRate;
+    connectionParameters.nRetransmissions = nTries;
+    connectionParameters.timeout = timeout;
+
+    if (llopen(connectionParameters) != 0) {
+        printf("ERROR\n");
+        return;
+    }
+
+    return;
+
     // TODO
 }
--- lab1/src/link_layer.c
+++ lab1/src/link_layer.c
@@ -2,9 +2,11 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
+#include <stdio.h>
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+#define BUF_SIZE 5
 
 ////////////////////////////////////////////////
 // LLOPEN
@@ -14,6 +16,59 @@ int llopen(LinkLayer connectionParameters)
     if (openSerialPort(connectionParameters.serialPort,
                        connectionParameters.baudRate) < 0)
     {
+        return -1;
+    }
+
+    if (connectionParameters.role == LlTx) {
+        unsigned char buf[BUF_SIZE] = {0};
+        buf[0] = 0x7E;
+        buf[1] = 0x03;
+        buf[2] = 0x03;
+        buf[3] = buf[1] ^ buf[2];
+        buf[4] = 0x7E;
+
+        int bytes = writeBytesSerialPort(buf, BUF_SIZE);
+
+        if (bytes < 5) {
+            printf("Error: fewer than 5 bytes written\n");
+            return -1;
+        }
+
+        printf("%d bytes written\n", bytes);
+        return 0;
+    }
+
+    else if (connectionParameters.role == LlRx) {
+        volatile int STOP = FALSE;
+        unsigned char buf[BUF_SIZE + 1] = {0}; // +1: Save space for the final '\0' char
+        int bytecounter = 0;
+
+        printf("RECEIVED SET FRAME:\n");
+
+        while (STOP == FALSE) {
+            int byte = readByteSerialPort(&buf[bytecounter]);
+            if (byte == -1) {
+                printf("Error: unable to read byte\n");
+                return -1;
+            } else if (byte == 0) {
+                printf("Error: no byte was received\n");
+                return -1;
+            }
+
+            printf("var = 0x%02X\n", (unsigned int)(buf[bytecounter]));
+
+            bytecounter++;
+            if (bytecounter == BUF_SIZE) {
+                buf[bytecounter] = '\0';
+                STOP = TRUE;
+                printf("Read %d bytes\n", bytecounter);
+            }
+        }
+
+        return 0;
+    }
+
+    else {
         return -1;
     }
 
